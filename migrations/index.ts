647046f2f3a51fb4983c9bbb1bb51524d5e3f225
Rach17 <rachediabderrahmane17@gmail.com>
2026-09-22 13:55:31 +0100

    style: prettier formatting for migrations index

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,11 +1,11 @@
-import * as migration_20260918_094355 from './20260918_094355';
-import * as migration_20260918_102126 from './20260918_102126';
-import * as migration_20260920_092548_drop_mcp_plugin_tables from './20260920_092548_drop_mcp_plugin_tables';
-import * as migration_20260920_104333_add_article_favorites from './20260920_104333_add_article_favorites';
-import * as migration_20260920_114739_user_activity_log_hook_fix from './20260920_114739_user_activity_log_hook_fix';
-import * as migration_20260922_092238_add_first_admin_created_action from './20260922_092238_add_first_admin_created_action';
-import * as migration_20260922_095148_password_reset_email_config from './20260922_095148_password_reset_email_config';
-import * as migration_20260922_124852_reset_email_origin_config from './20260922_124852_reset_email_origin_config';
+import * as migration_20260918_094355 from './20260918_094355'
+import * as migration_20260918_102126 from './20260918_102126'
+import * as migration_20260920_092548_drop_mcp_plugin_tables from './20260920_092548_drop_mcp_plugin_tables'
+import * as migration_20260920_104333_add_article_favorites from './20260920_104333_add_article_favorites'
+import * as migration_20260920_114739_user_activity_log_hook_fix from './20260920_114739_user_activity_log_hook_fix'
+import * as migration_20260922_092238_add_first_admin_created_action from './20260922_092238_add_first_admin_created_action'
+import * as migration_20260922_095148_password_reset_email_config from './20260922_095148_password_reset_email_config'
+import * as migration_20260922_124852_reset_email_origin_config from './20260922_124852_reset_email_origin_config'
 
 export const migrations = [
   {
@@ -46,6 +46,6 @@ export const migrations = [
   {
     up: migration_20260922_124852_reset_email_origin_config.up,
     down: migration_20260922_124852_reset_email_origin_config.down,
-    name: '20260922_124852_reset_email_origin_config'
+    name: '20260922_124852_reset_email_origin_config',
   },
-];
+]
